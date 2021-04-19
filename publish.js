28389e66fac13c32f1e3cd3b16c45565b1768429
Justin Bennett <zephraph@gmail.com>
2021-04-19 12:29:52 -0400

    vault backup: 2021-04-19 12:29:52

---
+++ publish.js
@@ -0,0 +1,9 @@
+(function (d, script) {
+  script = d.createElement("script");
+  script.type = "text/javascript";
+  script.async = true;
+  script.defer = true;
+  script.dataset.domain = "just-be.dev";
+  script.src = "https://plausible.io/js/plausible.js";
+  d.getElementsByTagName("head")[0].appendChild(script);
+})(document);
